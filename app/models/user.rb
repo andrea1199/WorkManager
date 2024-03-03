5fbe4e465d73041d9f07be43b42a67a406a8b53b
andrea1199 <andreavallonejr@gmail.com>
2024-03-03 01:49:40 +0100

    modifica visiva + ottenimento nome cognome data nascita da google in base alle impostazioni privacy

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ApplicationRecord
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create  do |user|
       user.email = provider_data.info.email
       user.password = Devise.friendly_token[0, 20]
-
+      user.nome = provider_data.info.first_name
+      user.cognome = provider_data.info.last_name
     end
   end
 
