a37d4e7f16526e5c66988a9f24290d782f70cb6a
andrea1199 <andreavallonejr@gmail.com>
2024-03-03 01:32:14 +0100

    signup funzionante

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
   # ...
+  #
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nome, :cognome, :data_di_nascita, :descrizione, :ruolo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nome, :cognome, :data_di_nascita, :descrizione, :ruolo])
+  end
 
   protected
 
