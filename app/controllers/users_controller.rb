e41e0d2409d120efb117872cc3ffddb00acc305c
scicamaru <virginia.guglielmi619@gmail.com>
2024-03-07 13:10:52 +0100

    aggiornata homepage + form informazioni + aggiunto controller users + aggiornare routes.rb (da rivedere)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def show
+    //visualizza l'utente
+  end
+
+  def edit
+    //modifica l'utente
+  end
+
+  def update
+    //aggiorna l'utente
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
   devise_for :users
   root 'pages#home'
   get 'pages/home'
@@ -16,6 +19,19 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'signup', to: 'devise/registrations#new'
   end
+
+  #ROUTES AGGIUNTE PER USER -> DA RIVEDERE - modifica fatta da @scicamaru
+
+  # Rotta per visualizzare il profilo dell'utente
+  # get '/users/:id', to: 'users#show', as: 'user'
+
+  # Rotta per visualizzare il modulo di modifica del profilo dell'utente
+  # get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+
+  # Rotta per gestire l'aggiornamento del profilo dell'utente
+  # patch '/users/:id', to: 'users#update'
+
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
