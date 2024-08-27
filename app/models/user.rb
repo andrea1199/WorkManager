902ba471f187630108d9b73a81f4c82ad94b0415
scicamaru <virginia.guglielmi619@gmail.com>
2024-08-27 17:11:17 +0200

    random scheduling e stipendi

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:github , :google_oauth2]
 
   belongs_to :company, optional: true
+  before_create :generate_scheduling, :generate_salaires
 
   # def self.from_omniauth(auth)
   #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -40,5 +41,32 @@ class User < ApplicationRecord
 
   def dipendente?
     ruolo == 'dipendente'
+  end
+
+  private 
+  def generate_scheduling
+    days_of_week = %w[Monday Tuesday Wednesday Thursday Friday]
+
+    days_of_week.each do |day|
+    DayScheduling.create!(
+      date: Date.parse(day),               # Giorno della settimana
+      start_work: '09:00',                 # Orario di inizio lavoro
+      end_work: '17:00',                   # Orario di fine lavoro
+      start_break: '13:00',                # Inizio pausa pranzo
+      end_break: '14:00',                  # Fine pausa pranzo
+      employee_id: self.id                 # Associa l'orario di lavoro all'utente corrente
+      )
+    end
+  end
+
+  def generate_salaires
+    # Genera 12 stipendi, uno per ogni mese del 2024
+    (1..12).each do |month|
+     Salaire.create!(
+    date: Date.new(2024, month, 1),   # La data dello stipendio: primo giorno di ogni mese
+    value: rand(2000..5000),          # Importo dello stipendio casuale tra 2000€ e 5000€
+    employee_id: self.id              # Associa lo stipendio all'utente corrente
+    )
+    end
   end
 end
