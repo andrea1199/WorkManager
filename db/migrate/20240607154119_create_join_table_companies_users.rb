7b75b7da742cb3241c595c5e6a1aec19d44eaa45
scicamaru <virginia,guglielmi619@gmail.com>
2024-08-30 11:06:25 +0200

    rimozione file inutili

--- db/migrate/20240607154119_create_join_table_companies_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableCompaniesUsers < ActiveRecord::Migration[7.1]
-  def change
-    create_join_table :companies, :users, table_name: :companies_users do |t|
-      t.index [:user_id, :company_id], name: 'index_companies_users_on_user'
-      t.index [:company_id, :user_id], name: 'index_companies_users_on_companies'
-    end
-  end
-end
--- db/migrate/20240607154306_create_join_table_employees_salaires.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableEmployeesSalaires < ActiveRecord::Migration[7.1]
-  def change
-    create_join_table :employees, :salaires, table_name: :employees_salaires do |t|
-      t.index [:salaire_id, :employee_id], name: 'index_employees_salaires_on_salaire'
-      t.index [:employee_id, :salaire_id], name: 'index_employees_salaires_on_employee'
-    end
-  end
-end
